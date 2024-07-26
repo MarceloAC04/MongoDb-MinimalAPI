e618b0e524e96dbb31cc8af6f7e7ea1f316f473a
Marcelo <marcelocardoso020304@gmail.com>
2024-07-26 16:47:29 -0300

    criado o repositorio

---
+++ minimal_api/minimalAPIMongo/Properties/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Properties.Domains;
+using minimalAPIMongo.Properties.Services;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Properties.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        /// <summary>
+        /// Armazena os dados de acesso da collection
+        /// </summary>
+        private readonly IMongoCollection<Product> _product;
+
+
+        /// <summary>
+        /// Construtor que recebe como dependência o obj da classe MongoDbService
+        /// </summary>
+        /// <param name="mongoDbService"></param>
+        public ProductController(MongoDbService mongoDbService)
+        {
+            //obtem a collection "product"
+            _product = mongoDbService.GetDatabase.GetCollection<Product>("product");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var products = await _product.Find(FilterDefinition<Product>.Empty).ToListAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Create([FromBody] Product newProduct)
+        {
+            try
+            {
+                await _product.InsertOneAsync(newProduct);
+
+                return StatusCode(201, newProduct);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Deletar(string id)
+        {
+            try
+            {
+                var product = _product.FindOneAndDeleteAsync(p => p.Id == id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("id")] 
+        public async Task<ActionResult<Product>> GetById(string id)
+        {
+            try
+            {
+                var product = await _product.FindAsync(p => p.Id == id).T;
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(product);
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ minimal_api/minimalAPIMongo/Properties/Domains/Product.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace minimalAPIMongo.Properties.Domains
+{
+    public class Product
+    {
+        //define que esta prop é Id do objeto
+        [BsonId]
+        //define o nome do campo no MongoDb como "_id" e o tipo como "ObjectId"
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string? Name { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+
+
+        //adiciona um dicionário para atributos adicionais
+        public Dictionary<string, string> AdditionalAttributes { get; set; }
+
+        /// <summary>
+        /// Ao ser instanciando um obj da classe Product, o atributo AdditionalAttributes ja
+        /// vira com um novo dicionário e portanto habilitado para adicionar + atributos
+        /// </summary>
+        public Product()
+        {
+            AdditionalAttributes = new Dictionary<string, string>();
+        }
+    }
+}
